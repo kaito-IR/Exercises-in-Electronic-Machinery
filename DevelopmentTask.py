b94017b3e648787629735fd7921bafff5f120260
kaito-IR <78997423+kaito-IR@users.noreply.github.com>
2022-04-20 15:35:16 +0900

    Update DevelopmentTask.py

--- DevelopmentTask.py
+++ DevelopmentTask.py
@@ -1,10 +1,13 @@
 import cv2
 import numpy as np
 import time
-
 cap = cv2.VideoCapture(0)
+width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+print(width,height)
 while(1):
     ret,frame = cap.read()
+    frame = cv2.resize(frame,dsize=(int(width/2),int(height/2)))
     # バイラテラルフィルタ
     before = frame.copy()
     beforeTime = time.perf_counter()
