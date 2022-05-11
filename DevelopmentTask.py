b7c57208865d0ec05babfc85d5560a159cccafb3
kaito-IR <78997423+kaito-IR@users.noreply.github.com>
2022-05-11 15:45:32 +0900

    Ver.β

--- DevelopmentTask.py
+++ DevelopmentTask.py
@@ -1,13 +1,9 @@
 import cv2
-import numpy as np
 import adjust
-import time
 import datetime
 adjust = adjust.adjust()
 face_cascade_path = 'haarcascade_frontalface_default.xml'
 face_cascade = cv2.CascadeClassifier(face_cascade_path)
-#smile_cascade_path = 'haarcascade_smile.xml'
-#smile_cascade = cv2.CascadeClassifier(smile_cascade_path)
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('H', '2', '6', '4'));
 cap.set(cv2.CAP_PROP_BUFFERSIZE, 5)
@@ -15,6 +11,12 @@ width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
 height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 Kf = 30
 Kf_Max = 50
+ratio = 0.05
+ChangeGrayFrame = False
+ChangeBinaryFrame = False
+ChangeAvatarFrame = False
+ChangeHSVFrame = False
+ChangeMosaicFrame = False
 
 def BeautifulSkinFilter():
     ret,frame = cap.read()
@@ -22,14 +24,28 @@ def BeautifulSkinFilter():
     # バイラテラルフィルタ
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+    if ChangeGrayFrame:
+        frame = gray
+    elif ChangeBinaryFrame:
+        ret2,frame = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU)
+    elif ChangeAvatarFrame:
+        frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+    elif ChangeHSVFrame:
+        frame = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
     if len(faces) != 0:
-        for x, y, w, h in faces:
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            face = frame[y: y + h, x: x + w]
-            face = adjust.adjust(face,1.1,Kf)
-            frame = cv2.blur(frame,(2,2))
-            dst = cv2.bilateralFilter(face, Kf, sigmaColor=1.2*(Kf+70), sigmaSpace=1.2*(Kf+20))
-            frame[y:y + h,x: x + w] = dst
+        if ChangeMosaicFrame:
+            for x,y,w,h in faces:
+                small = cv2.resize(frame[y: y + h, x: x + w], None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST)
+                frame[y: y + h, x: x + w] = cv2.resize(small, (w, h), interpolation=cv2.INTER_NEAREST)
+        else:
+            for x, y, w, h in faces:
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                face = frame[y: y + h, x: x + w]
+                face_gray = gray[y: y + h, x: x + w]
+                face = adjust.adjust(face,1.1,Kf)
+                frame = cv2.blur(frame,(2,2))
+                dst = cv2.bilateralFilter(face, Kf, sigmaColor=1.2*(Kf+70), sigmaSpace=1.2*(Kf+20))
+                frame[y:y + h,x: x + w] = dst
     return frame
 
 def ChangeValueTrackbar(position):
@@ -50,6 +66,16 @@ while(1):
     elif key == ord('s'):
         dt_now = datetime.datetime.now()
         cv2.imwrite(""+dt_now.strftime('%Y年%m月%d日 %H:%M:%S')+".png",frame)
+    elif key == ord('g'):
+        ChangeGrayFrame = not ChangeGrayFrame
+    elif key == ord('b'):
+        ChangeBinaryFrame = not ChangeBinaryFrame
+    elif key == ord('a'):
+        ChangeAvatarFrame = not ChangeAvatarFrame
+    elif key == ord('h'):
+        ChangeHSVFrame = not ChangeHSVFrame
+    elif key == ord('m'):
+        ChangeMosaicFrame = not ChangeMosaicFrame
         
 cv2.destroyAllWindows()
 cap.release()
