fd595959952a4c4ac794f614de36f350e84ce12f
kaito-IR <78997423+kaito-IR@users.noreply.github.com>
2022-05-11 14:31:56 +0900

    hoge

--- DevelopmentTask.py
+++ DevelopmentTask.py
@@ -10,50 +10,43 @@ face_cascade = cv2.CascadeClassifier(face_cascade_path)
 #smile_cascade = cv2.CascadeClassifier(smile_cascade_path)
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('H', '2', '6', '4'));
+cap.set(cv2.CAP_PROP_BUFFERSIZE, 5)
 width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
 height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-BeautifulSkinFilter = True
-while(1):
+Kf = 30
+Kf_Max = 50
+
+def BeautifulSkinFilter():
     ret,frame = cap.read()
     frame = cv2.resize(frame,dsize=(int(width/2),int(height/2)))
     # バイラテラルフィルタ
-    before = frame.copy()
-    if BeautifulSkinFilter:
-        beforeTime = time.perf_counter()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
-        #smiles = smile_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
-        if len(faces) != 0:
-            for x, y, w, h in faces:
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-                face = frame[y: y + h, x: x + w]
-                face_gray = gray[y: y + h, x: x + w]
-                face = adjust.adjust(face,1.1,30.0)
-                frame = cv2.blur(frame,(2,2))
-                dst = cv2.bilateralFilter(face, 30, sigmaColor=120, sigmaSpace=80)
-                frame[y:y + h,x: x + w] = dst
-        #elif len(smiles) != 0:
-        #    for x, y, w, h in smiles:
-        #        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        #        face = frame[y: y + h, x: x + w]
-        #        face_gray = gray[y: y + h, x: x + w]
-        #        face = adjust.adjust(face,1.1,30.0)
-        #        #frame = cv2.blur(frame,(2,2))
-        #        dst = cv2.bilateralFilter(face, 30, sigmaColor=120, sigmaSpace=80)
-        #        frame[y:y + h,x: x + w] = dst
-        #print(smiles)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+    if len(faces) != 0:
+        for x, y, w, h in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            face = frame[y: y + h, x: x + w]
+            face = adjust.adjust(face,1.1,Kf)
+            frame = cv2.blur(frame,(2,2))
+            dst = cv2.bilateralFilter(face, Kf, sigmaColor=1.2*(Kf+70), sigmaSpace=1.2*(Kf+20))
+            frame[y:y + h,x: x + w] = dst
+    return frame
+
+def ChangeValueTrackbar(position):
+    global Kf
+    Kf = position
+    frame = BeautifulSkinFilter()
     cv2.imshow("after",frame)
-    afterTime = time.perf_counter()
-    cv2.imshow("before",before)
-    print(afterTime - beforeTime)
+
+cv2.namedWindow("after")
+cv2.createTrackbar("FilterStrength","after",Kf,Kf_Max,ChangeValueTrackbar)
+while(1):
+    # バイラテラルフィルタ
+    frame = BeautifulSkinFilter()
+    cv2.imshow("after",frame)
     key = cv2.waitKey(1)
     if key == 27:#escキーが押された時
         break
-    elif key == ord('f'):
-        if BeautifulSkinFilter:
-            BeautifulSkinFilter = False
-        else:
-            BeautifulSkinFilter = True
     elif key == ord('s'):
         dt_now = datetime.datetime.now()
         cv2.imwrite(""+dt_now.strftime('%Y年%m月%d日 %H:%M:%S')+".png",frame)
